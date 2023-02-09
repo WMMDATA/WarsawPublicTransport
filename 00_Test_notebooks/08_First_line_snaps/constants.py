f12c7221bf857476294d4567372176f882abfa73
MariuszAndziak <43908826+MariuszAndziak@users.noreply.github.com>
2023-02-09 20:58:55 +0100

    Add files via upload

---
+++ 00_Test_notebooks/08_First_line_snaps/constants.py
@@ -0,0 +1,12 @@
+''' This module defines project-level constants'''
+
+### Main files and folders ###
+TIMETABLES_FOLDER = 'data/Timetables/'
+GPS_POSITIONS_FOLDER = 'data/Positions/'
+SEQUENCES_FOLDER = 'data/Sequences/'
+ROUTES_GEOMETRY_FILE = 'data/Lines/Trams_points_vertices.txt'
+
+DATE = '13_01_2023' 
+
+TRAM_NUMBER = 33 
+TRAM_ICON = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8ghk-iBncj_HnWVMjR623u97xyVwnGLmPg&usqp=CAU"
